cdb38ef562dc1de609cefadb4d0a3ba3a0953edb
tijanja007 <adetunji.akinde@techadvance.ng>
2018-09-23 15:43:44 +0100

    edit 5

--- speakers.php
+++ speakers.php
@@ -13,14 +13,14 @@ $db = new mysqli('localhost', 'root', 'Project123', 'ican2018');
       $file_name = $_FILES['image']['name'];
       $file_size =$_FILES['image']['size'];
       $file_tmp =$_FILES['image']['tmp_name'];
-      $file_type=$_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      //$file_type=$_FILES['image']['type'];
+     // $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
       
-      $expensions= array("jpeg","jpg","png");
+      //$expensions= array("jpeg","jpg","png");
       
-      if(in_array($file_ext,$expensions)=== false){
-         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-      }
+//      if(in_array($file_ext,$expensions)=== false){
+//         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+//      }
       
       if($file_size > 2097152){
          $errors[]='File size must be excately 2 MB';
