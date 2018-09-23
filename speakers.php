778cb82bfe658d6d149466329154727b26a8865a
tijanja007 <adetunji.akinde@techadvance.ng>
2018-09-23 15:24:32 +0100

    edit 1

--- speakers.php
+++ speakers.php
@@ -8,7 +8,7 @@
         <input type="text" name="speakerName" placeholder="Name"/><br>
         <input type="text" name="speakerRanks" placeholder="Credentials"/><br>
         <input type="text" name="speakerSession" placeholder="Plenary"/><br>
-        <textarea cols="5" rows="4" name="profile_text" placeholder="Profile Text"/>
+        <textarea cols="5" rows="4" name="profile_text" placeholder="Profile Text"></textarea>
         <input type="file" name="image"/>
         <input type="submit" name="uploadSpeaker" title="Upload" />
         </form>
