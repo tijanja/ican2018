c25c2528263102119b5e58d3728f97d48327035b
tijanja007 <adetunji.akinde@techadvance.ng>
2018-09-23 15:26:42 +0100

    edit 3

--- speakers.php
+++ speakers.php
@@ -8,7 +8,7 @@
         <input type="text" name="speakerName" placeholder="Name"/><br>
         <input type="text" name="speakerRanks" placeholder="Credentials"/><br>
         <input type="text" name="speakerSession" placeholder="Plenary"/><br>
-        <textarea cols="5" rows="4" name="profile_text" placeholder="Profile Text"></textarea><br>
+        <textarea cols="20" rows="4" name="profile_text" placeholder="Profile Text"></textarea><br>
         <input type="file" name="image"/><br>
             <input type="submit" name="uploadSpeaker" title="Upload" /><br>
         </form>
