62cd39650a32ca8530cdd6c6b474235a3fc4612c
root <root@ip-172-31-28-224.eu-west-1.compute.internal>
2018-09-23 13:57:10 +0000

    first commit using this reo for 48th conference too

---
+++ controller/ICAN.php
@@ -0,0 +1,16 @@
+<?php
+include_once './model/include.php';
+
+class ICAN {
+     private $_params;
+    
+    function __construct($params)
+    {
+        $this->_params = $params;
+    }
+    
+    function login()
+    {
+        
+    }
+}
