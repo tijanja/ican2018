a92f18b1ca27cfcfed25bbf0b832a43a18e3422b
tijanja007 <adetunji.akinde@techadvance.ng>
2018-09-23 16:14:01 +0100

    edit upload folde added

--- speakers.php
+++ speakers.php
@@ -1,7 +1,7 @@
 <?php
 
 $db = new mysqli('localhost', 'root', 'Project123', 'ican2018');
-    if(isset($_FILES['image'])){
+    if(isset($_POST['uploadSpeaker'])){
         
        $name = trim($_POST['speakerName']);
        $rank = trim($_POST['speakerRanks']);
@@ -16,18 +16,22 @@ $db = new mysqli('localhost', 'root', 'Project123', 'ican2018');
       //$file_type=$_FILES['image']['type'];
      // $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
       
-      //$expensions= array("jpeg","jpg","png");
-      
-//      if(in_array($file_ext,$expensions)=== false){
-//         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-//      }
+
       
       if($file_size > 2097152){
          $errors[]='File size must be excately 2 MB';
       }
       
       if(empty($errors)==true){
-         move_uploaded_file($file_tmp,"images/".$file_name);
+          $target_path = "images/";
+         $target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+
+        if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+            echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
+            " has been uploaded";
+        } else{
+            echo "There was an error uploading the file, please try again!";
+        }
          
        $db->query("INSERT INTO speakers VALUES('','$name','$file_name','$penary','$rank','0')");
          echo "Success";
@@ -44,7 +48,7 @@ $db = new mysqli('localhost', 'root', 'Project123', 'ican2018');
         <input type="text" name="speakerRanks" placeholder="Credentials"/><br>
         <input type="text" name="speakerSession" placeholder="Plenary"/><br>
         <textarea cols="20" rows="4" name="profile_text" placeholder="Profile Text"></textarea><br>
-        <input type="file" name="image"/><br>
+        <input type="file" name="uploadedfile"/><br>
             <input type="submit" name="uploadSpeaker" title="Upload" /><br>
         </form>
     </body>
