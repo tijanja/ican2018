2a7c609c80786ef98cfef50a1dce55c42d5863a0
tijanja007 <adetunji.akinde@techadvance.ng>
2018-09-24 04:19:04 +0100

    edit upload folde added 1

--- speakers.php
+++ speakers.php
@@ -7,7 +7,7 @@ $db = new mysqli('localhost', 'root', 'Project123', 'ican2018');
        echo $name = trim($_POST['speakerName']);
        echo $rank = trim($_POST['speakerRanks']);
        echo $penary = trim($_POST['speakerSession']);
-       echo $profile = trim($_POST['profile_text']);
+       echo $profile = addcslashes("'",trim($_POST['profile_text']));
        
        
         //get and save speakers images
