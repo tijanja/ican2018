ae405877d17be7b5c7b95bed65f770080725c11d
tijanja007 <adetunji.akinde@techadvance.ng>
2018-09-24 02:32:29 +0100

    edit upload folde added 1

--- speakers.php
+++ speakers.php
@@ -25,7 +25,7 @@ $db = new mysqli('localhost', 'root', 'Project123', 'ican2018');
       
       if(empty($errors)==true){
           $target_path = "images/";
-         echo $target_path = $target_path . basename( $_FILES['image']['name']); 
+         echo $target_path = $target_path . basename( $_FILES["image"]["name"]); 
 
 //        if(move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
 //            echo "The file ".  basename( $_FILES['image']['name']). 
